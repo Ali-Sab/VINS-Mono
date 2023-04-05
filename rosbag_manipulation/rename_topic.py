3228321485bf1574a0347599fb5ed7e10188cb08
Alison Sabuwala <alison.sabuwala1024@gmail.com>
2023-04-05 22:41:51 +0000

    enable fast relocalization

--- rosbag_manipulation/rename_topic.py
+++ rosbag_manipulation/rename_topic.py
@@ -1,4 +1,4 @@
 from rosbag import Bag
-with Bag('bahen_legal_new.bag', 'w') as Y:
-    for topic, msg, t in Bag('bahen_legal.bag'):
+with Bag('bahen_fast_imu2_vins.bag', 'w') as Y:
+    for topic, msg, t in Bag('bahen_fast_imu2.bag'):
         Y.write('/cam0/image_raw' if topic == '/tello/image_raw' else topic, msg, t)
